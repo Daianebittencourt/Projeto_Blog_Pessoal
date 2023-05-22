b9f6b4904e4579cac0db49018011e7ab6f07b61a
Daiane Bittencourt <daibittencourt2000@gmail.com>
2023-05-22 00:37:24 -0300

    Componentes Navbar, Home, Footer e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-import Home from "./pagina/home/Home"
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import Footer from "./components/estaticos/footer/Footer";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Home from "./pagina/home/Home";
+import "./App.css";
+
+import Login from "./pagina/login/Login";
+
+
 
 function App() {
-  return (
-    <>
-      <>Home</>
-    </>
+  return(
+    <BrowserRouter>
+    <Navbar />
+    <div style={{ minHeight: '100vh' }}>
+    <Routes>
+    <Route path="/home" element={<Home />} />
+    <Route path="/" element={<Login />} />
+    <Route path="/login" element={<Login />} />
+    </Routes>
+    </div>
+    <Footer />
+    </BrowserRouter>
+    
   );
 }
 
