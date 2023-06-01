713c753e65de00a9bfa0ae993ea3542c1db8c7b4
Daiane Bittencourt <daibittencourt2000@gmail.com>
2023-06-01 11:27:01 -0300

    Deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://blogpessoaldaianebittencourt.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 }) 
 
 export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
@@ -13,3 +13,9 @@ export const login = async(url:any, dados:any, setDado:any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token) 
 }
+
+export const busca = async(url: any,setDado: any, header: any) => { 
+    const resposta = await api.get(url,header)
+    setDado(resposta.data)
+}
+
