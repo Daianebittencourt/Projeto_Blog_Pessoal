c5a914f6adce7439983e70b014c65f40431a38d8
Daiane Bittencourt <daibittencourt2000@gmail.com>
2023-06-01 23:53:30 -0300

    Resolução de problemas e conflitos

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,6 @@ function App() {
 
           <Route path="/" element={<Login />} />
 
-
-
           <Route path="/login" element={<Login />} />
 
           <Route path="/home" element={<Home />} />
--- src/components/postagens/tabPostagem/TabPostagem.tsx
+++ src/components/postagens/tabPostagem/TabPostagem.tsx
@@ -3,6 +3,7 @@ import { AppBar, Tab, Tabs, Typography } from '@material-ui/core';
 import {Box} from '@mui/material';
 import { TabContext, TabPanel } from '@material-ui/lab';
 import './TabPostagem.css';
+import ListaPostagem from '../listaPostagem/ListaPostagem';
 
 function TabPostagem() {
     const [value, setValue] = useState('1')
